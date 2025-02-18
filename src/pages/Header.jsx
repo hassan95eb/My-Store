cec35d8b84e52f0990454b01ecd67aed5f2baee3
hassan95eb <19h.amini95@gmail.com>
2025-02-18 22:44:56 +0330

    not complete

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,58 @@ import Navbar from "./Navbar";
 const Header = () => {
   return (
     <div>
-      <Navbar />
+      <header className="flex shadow-lg py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
+        <div className="flex flex-wrap items-center justify-between gap-4 w-full">
+          <a
+            href="javascript:void(0)"
+            className="lg:absolute max-lg:left-10 lg:top-2/4 lg:left-2/4 lg:-translate-x-1/2 lg:-translate-y-1/2 max-sm:hidden"
+          >
+            <img
+              src="https://readymadeui.com/readymadeui.svg"
+              alt="logo"
+              className="w-36"
+            />
+          </a>
+          <a href="javascript:void(0)" className="hidden max-sm:block">
+            <img
+              src="https://readymadeui.com/readymadeui-short.svg"
+              alt="logo"
+              className="w-9"
+            />
+          </a>
+
+          <Navbar />
+
+          <div className="flex items-center ml-auto space-x-6">
+            <button className="font-semibold text-[15px] border-none outline-none">
+              <a
+                href="javascript:void(0)"
+                className="text-[#007bff] hover:underline"
+              >
+                Login
+              </a>
+            </button>
+            <button className="px-4 py-2 text-sm rounded-sm font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+              Sign up
+            </button>
+
+            <button id="toggleOpen" className="lg:hidden">
+              <svg
+                className="w-7 h-7"
+                fill="#333"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
